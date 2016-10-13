a48d54381a4117d50c2f3b55477786920f09f1b7
z416175 <francis.kabinoff@gmail.com>
2016-10-13 19:44:40 -0400

    improved styling

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { CardsComponent } from './cards.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule.forRoot(),
     RouterModule.forChild([
       { path: '', component: CardsComponent }
     ])
